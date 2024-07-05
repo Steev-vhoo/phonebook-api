7ed986db85cbff053168b9b3805acc913a21e0b9
Steev Vhoo <osteevvhoo@gmail.com>
2024-07-05 15:14:55 +0000

    second commit

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,52 +1,78 @@
 import { ContactModel } from "../models/contacts.js";
 
 export const getContacts = async (req, res, next) => {
-    try {
-        //Get all contacts from database
-        const allContacts = await ContactModel.find()
-        //Return all contacts
-        res.status(200).json(allContacts)
-    } catch (error) {
-        next(error);
-    }
-}
+  try {
+    // Get query params- the structuring
+    // const {
+    //   filter = "{}",
+    //   sort = "{}",
+    //   fields = "{}",
+    //   limit = 10,
+    //   skip = 0
+    // } = req.query;
+    // if (contact) {
+    // Get all filtered contacts from database
+    // const allContacts = await ContactModel
+    //   .find(JSON.parse(filter))
+    //   .sort(JSON.parse(sort))
+    //   .select(JSON.parse(fields))
+    //   .limit(limit)
+    //   .skip(skip);
+    // Return response
+    // res.status(200).json(allContacts);
+    // } else if (contact) {
+    //Get all contacts from database
+    const allContacts = await ContactModel.find()
+    //Return all contacts
+    res.status(200).json(allContacts)
+    // } else {
+    //Get one contacts from database
+    // const allContacts = await ContactModel.find(req.params.id)
+    //Return all contacts
+    // res.status(200).json(allContacts)
 
+  } catch (error) {
+    next(error);
+  }
+}
 export const addContacts = async (req, res, next) => {
 
-    try {
-        //Add contacts to database
-       const newContacts = await ContactModel.create(req.body);
-        //Return response
-        res.status(200).json(newContacts) //Update
-    } catch (error) {
-        next(error);
-    }
-}
-
-
-
-export const deleteContact =  async (req, res, next) => {
-   try {
-    //Delete contact by ID
-    const deletedContact =  await ContactModel.findByIdAndDelete(req.params.id)
+  try {
+    //Add contacts to database
+    const newContacts = await ContactModel.create({
+      ...req.body
+    });
     //Return response
-     res.status(200).json(deletedContact)
-
-   } catch (error) {
-    next(error)
-   }
+    res.status(201).json(newContacts) //Update
+  } catch (error) {
+    next(error);
+  }
 }
 
-export const updateContact =  async (req, res) => {
-    try {
-      const updatedAddress = await Address.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        { new: true }
-      );
-      res.json(updatedAddress);
-    } catch (err) {
-      res.status(400).json({ error: err.message });
-    }
+
+
+export const deleteContact = async (req, res, next) => {
+  try {
+    //Delete contact by ID
+    const deletedContact = await ContactModel.findByIdAndDelete(req.params.id)
+    //Return response
+    res.status(200).json(deletedContact)
+
+  } catch (error) {
+    next(error)
   }
+}
+
+export const updateContact = async (req, res) => {
+  try {
+    const updatedAddress = await Address.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.json(updatedAddress);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+}
 
--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import contactRouter from "./routes/contacts.js"
+import expressOasGenerator from "express-oas-generator";
 
 //Connect to database
 await mongoose.connect(process.env.mongo_url);
+console.log("Connected to Contact Database")
 
 
 
 //Create express app
 const app = express();
-
+expressOasGenerator.handleResponses(app, {
+  alwaysServeDocs: true,
+  tags: ['contacts'],
+  mongooseModels: mongoose.modelNames(),
+})
 //Apply middleware
 app.use(express.json());
 
  
 //Use Routes
 app.use(contactRouter);
+expressOasGenerator.handleRequests();
+app.use((req, res) => res.redirect('/api-docs'));
+
  
 
  
